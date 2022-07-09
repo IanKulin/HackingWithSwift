fc0b40dd29848792d2c10a3a8da5e5d76bc888c7
IanKulin <108404298+IanKulin@users.noreply.github.com>
2022-07-10 07:43:12 +0800

    Create CheckPoint03.swift

---
+++ CheckPoint03.swift
@@ -0,0 +1,23 @@
+/*
+ The problem is called fizz buzz, and has been used in job interviews, university entrance tests, and more for as long as I can remember. 
+Your goal is to loop from 1 through 100, and for each number:
+ 
+ If it’s a multiple of 3, print “Fizz”
+ If it’s a multiple of 5, print “Buzz”
+ If it’s a multiple of 3 and 5, print “FizzBuzz”
+ Otherwise, just print the number.
+ */
+
+for i in 1...100 {
+    let isMultOfThree = (i % 3 == 0)
+    let isMultOfFive = (i % 5 == 0)
+    if (isMultOfFive && isMultOfThree) {
+        print("FizzBuzz")
+    } else if isMultOfThree {
+        print("Fizz")
+    } else if isMultOfFive {
+        print("Buzz")
+    } else {
+        print(i)
+    }
+}
