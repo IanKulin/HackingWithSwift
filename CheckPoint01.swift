14b8ea32d94d7ac8ff98f7ab4d953c3d5fbf8a2e
IanKulin <108404298+IanKulin@users.noreply.github.com>
2022-07-10 08:04:48 +0800

    Create CheckPoint01.swift

---
+++ CheckPoint01.swift
@@ -0,0 +1,11 @@
+/*
+ Your goal is to write a Swift playground that:
+ 
+ Creates a constant holding any temperature in Celsius.
+ Converts it to Fahrenheit by multiplying by 9, dividing by 5, then adding 32.
+ Prints the result for the user, showing both the Celsius and Fahrenheit values.
+ */
+
+let celciusTemperature = 36.7
+let fahrenheitTemperature = (celciusTemperature*9/5)+32
+print("Celcius:\(celciusTemperature)° Fahrenheit:\(fahrenheitTemperature)°")
